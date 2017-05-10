d2a0a82cab187290f8baeca4ff3ac854f013986c
helloworld <sthupukari@gmail.com>
2017-05-10 03:26:47 -0400

    File selection + Upload file to S3 directly

---
+++ public/js/dashboard/upload.js
@@ -0,0 +1,37 @@
+function upload(file, signed_request, url, done) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", signed_request);
+    xhr.setRequestHeader("x-amz-acl", "public-read");
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            done();
+        }
+    };
+
+    xhr.send(file);
+}
+
+function sign_request(file, done) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/sign?file_name=" + file.name + "&file_type=" + file.type);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            done(response);
+        }
+    };
+
+    xhr.send();
+}
+
+document.getElementById("file-selection").onchange = function() {
+    var file = document.getElementById("file-selection").files[0];
+    if (!file) return;
+
+    sign_request(file, function(response) {
+        upload(file, response.signed_request, response.url, function() {
+            console.log(response);
+        });
+    });
+};
