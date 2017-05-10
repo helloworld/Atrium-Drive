ca859746d7c27efe0036690dc82054df73147fa0
helloworld <sthupukari@gmail.com>
2017-05-10 03:46:23 -0400

    Render list of user's files

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,26 +1,31 @@
 let File = require("../models/file.model");
-let extension = require('file-extension');
+let extension = require("file-extension");
 
 // -- Dashboard Controller -----------------------------------------------------
 
 // -- GET /dashboard -----------------------------------------------------------
 exports.index = function(req, res) {
-    res.render('dashboard', {
-        title: 'Atrium-Drive'
-    });
+	File.find({ user_id: req.user._id }, function(err, files) {
+		console.log(files);
+		if (err) throw err;
+		res.render("dashboard", {
+			title: "Atrium-Drive",
+			files: files
+		});
+	});
 };
 
 // -- POST /dashboard/addFile --------------------------------------------------
 exports.addFile = function(req, res) {
-    let new_file = new File({
-        filename: req.body.filename,
-        filetype: extension(req.body.filename),
-        url: req.body.url,
-        user_id: req.user._id,
-    });
+	let new_file = new File({
+		filename: req.body.filename,
+		filetype: extension(req.body.filename),
+		url: req.body.url,
+		user_id: req.user._id
+	});
 
-    new_file.save(function(err) {
-        if (err) throw err;
-        res.json(new_file);
-    });
+	new_file.save(function(err) {
+		if (err) throw err;
+		res.json(new_file);
+	});
 };
