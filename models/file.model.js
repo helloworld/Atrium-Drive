8076ae4edfb082aff93ce8d5702c63127efe666b
helloworld <sthupukari@gmail.com>
2017-05-10 03:36:54 -0400

    File model + schema

---
+++ models/file.model.js
@@ -0,0 +1,31 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+const schemaOptions = {
+  timestamps: true,
+  toJSON: {
+    virtuals: true
+  }
+};
+
+const FileSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String, 
+      required: true, 
+    },
+    created_at: Date,
+    user_id: Schema.Types.ObjectId
+  },
+  schemaOptions
+);
+
+module.exports = mongoose.model("File", FileSchema);
